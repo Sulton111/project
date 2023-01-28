3c3335e00b94b4f84f52f9f37966f09fd26924e5
Sultonboy <radjabovsultonboy98@gmail.com>
2023-01-28 05:32:19 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,73 @@
+// 11-lesson
+//alert("hello");
+
+// const result= confirm("Are you here");
+// console.log(result);
+
+// const answer= +prompt(" vam est 18?");
+// // console.log(answer);
+// console.log(answer+10);
+// // vse informastsii ot polzovatelya budet v vide strok
+
+/*
+const answers= [];
+
+answers[0]= prompt("kak vawe imya?",'');
+answers[1]= prompt("kak vawa familiya?",'');
+answers[2]= prompt("skolko vam let?",'');
+console.log(typeof(answers));
+*/
+// document.write(answers);
+
+// 12-lesson
+
+/*
+const category= "toys";
+
+console.log('https://someurl.com/'+category);
+
+const user= "ivan";
+
+alert(`Privet, ${user}`);
+*/
+
+// 13-lesson opertori
+
+console.log('arr'+'-object');
+console.log(4+ +"5");
+
+
+let incr=10,
+ decr= 10;
+
+ incr++;
+ decr--;
+ console.log(incr);
+ console.log(decr);
+
+ /*
+ 
+let incr=10,
+ decr= 10;
+
+ incr;
+ decr;
+ v etom slusae plusi minusi ne izmenyat  znaceniuya;
+ console.log(incr++);
+ console.log(decr--);
+
+ let incr=10,
+ decr= 10;
+
+ incr;
+ decr;
+ v etom slusae plusi minusi izmenyat  znaceniuya;
+ console.log(++incr);
+ console.log(--decr);   
+ */
+
+ const isChecked =true,
+       isClose= false;
+       console.log(isChecked && isClose);
+       console.log(isChecked || isClose);
+       
